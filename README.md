276cba8d7647940f3bed11e73c6268e3728cff56
Krimeas <krimeass@gmail.com>
2022-10-09 16:51:29 -0500

    added more comments, completed readme, worked on output of pulled API data, continued css changes for sizing. Media resizing issues need correction still.

--- README.md
+++ README.md
@@ -1,10 +1,53 @@
 # weather-dashboard
-A dashboard adding cities to track weather.
+Project to build a Scheduler
+
+## sixth Challenge for Bootcamp
+Complete a weather dashboard for searching city names to output weather data for current and next five days.
+  
+
+## Weather Dashboard
+When page loads you can click into the search city input box and type a name.  That name will then pull weather data from openweather API.  
+
+https://krimeas.github.io/weather-dashboard/
+
+![Portfolio](/assets/home_page.JPG)
+
+
+------------------------------------------------------------
+
+### Task assigned:
+  1. Make HTML, CSS, Jabvascript files.
+  2. when searching city, must return weather forecast into boxes for current, and next five days
+  3. Searched city must be logged into saved/previous searches.
+  4. Search of city must log city name and other data after search.
+  5. Temp, wind speed, and humidity must log for each coty searched.
+  6. clicking on previously searched city must return previous history from local storage.
+  
+
+### Technologies used in this project were:
+  1. HTML
+  2. CSS
+  3. VSCode
+  4. Bootstrap
+  5. momentjs
+  6. OpenWeather API
+  6. Git bash for windows
+  7. JavaScript & Jquery
+  8. Chrome Web Broswer & Dev Tools
+
+------------------------------------------------------------
+
+#### Additional Sources:
+  1. Working with input box - https://www.delftstack.com/howto/javascript/javascript-get-input-value/
+  2. Small code segments pulled from previous assignments
+  3. Assistance with API - https://w3collective.com/fetch-display-api-data-javascript/
+
+
+
+Task details supplied by UT Austin Coding Bootcamp.
+
+This code follows the MIT License!
 
 
 
 
-
-
-
-https://www.delftstack.com/howto/javascript/javascript-get-input-value/
--- assets/script.js
+++ assets/script.js
@@ -4,52 +4,41 @@
 //variables need to add data to html when search is hit.
 // need lat and lon variable - need to pull lat and lon and put into the city name variable when a city name is typed in.
 
-
+//posts date for the five days forecast.
 var oneDate = moment().add(1, 'days').format("M/D/YYYY");
-$("#day1").text(oneDate);
+$("#day1").text("(" + oneDate + ")");
 var twoDate = moment().add(2, 'days').format("M/D/YYYY");
-$("#day2").text(twoDate);
+$("#day2").text("(" + twoDate + ")");
 var threeDate = moment().add(3, 'days').format("M/D/YYYY");
-$("#day3").text(threeDate);
+$("#day3").text("(" + threeDate + ")");
 var fourDate = moment().add(4, 'days').format("M/D/YYYY");
-$("#day4").text(fourDate);
+$("#day4").text("(" + fourDate + ")");
 var fiveDate = moment().add(5, 'days').format("M/D/YYYY");
-$("#day5").text(fiveDate);
+$("#day5").text("(" + fiveDate + ")");
 
-
+//Sets API key and variables for the pushed previous searches.
 var APIkey = "b8f205c7b5d3ff424774477d455341bc";
 var formEl = $('#citySearch');
 var cityListEl = $('#city-list');
 
 document.querySelector("#subbtn").addEventListener("click", submitForm);
 
+// function to run the submit button and pull API data from openweather.
 function submitForm(event) {
   event.preventDefault();
-
   var cityNameEl = $('#city-name');
-
   nameInput = cityNameEl.val();
-  
   logCity(nameInput);
-  console.log(nameInput);
 
-var todaysDate = moment("2021-06-15").format("M/D/YYYY");
-$("#today").text(todaysDate);
+  var todaysDate = moment("2021-06-15").format("M/D/YYYY");
+  $("#today").text("(" + todaysDate + ")");
 
 
 
   var queryURL1 = "http://api.openweathermap.org/data/2.5/weather?q=" + nameInput + "&appid=" + APIkey;
-  // var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIkey;
-  var latnlon = 'http://api.openweathermap.org/geo/1.0/direct?q=' + nameInput + '&limit=5&appid=' + APIkey;
-  var citylatnlon = latnlon;
-
-  var temp = 0;
-  console.log(temp);
-  var wind = 0;
-  console.log(temp);
-  var humidity =  0;
-  console.log(temp);
-
+  var queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + nameInput + "&appid=" + APIkey;
+  // var latnlon = 'http://api.openweathermap.org/geo/1.0/direct?q=' + nameInput + '&limit=5&appid=' + APIkey;
+  // var citylatnlon = latnlon;
 
 
 
@@ -63,24 +52,75 @@ $("#today").text(todaysDate);
   });
   localStorage.setItem("queryURL1", JSON.stringify(queryURL1))
 
+  function daycurrent (data){
+    var tempcurrent = data.main.temp;
+
+    console.log(tempcurrent);
+    var windcurrent = data.wind.speed;
+
+    console.log(windcurrent);
+    var humiditycurrent =  data.main.humidity;
+
+    console.log(humiditycurrent);
+    var citycurrent =  data.name;
+    $("#city").text(citycurrent);
+    console.log(citycurrent);
+  }
+
 
 
 // For the 5 day forecast - currently unable to due to paywall.  
 // possible to generate arrays for 
-  // fetch(queryURL2, {
+  fetch(queryURL2, {
 
-  //   cache: 'reload',
-  // })
-  //   .then(function (response) {
-  //     return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log(data);
-  //   });
-  //   localStorage.setItem("queryURL2", JSON.stringify(queryURL2))
+    cache: 'reload',
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+    localStorage.setItem("queryURL2", JSON.stringify(queryURL2))
 
-// };
-}
+    function dayAhead (data){
+    var tempforecast1 = data.list[5].main.temp;
+    console.log(tempforecast1);
+    var windforecast1 = data.list[5].wind.speed;
+    console.log(windforecast1);
+    var humidityforecast1 =  data.list[5].main.humidity;
+    console.log(humidityforecast1);
+
+    var tempforecast2 = data.list[13].main.temp;
+    console.log(tempforecast2);
+    var windforecast2 = data.list[13].wind.speed;
+    console.log(windforecast2);
+    var humidityforecast2 =  data.list[13].main.humidity;
+    console.log(humidityforecast2);
+
+    var tempforecast3 = data.list[21].main.temp;
+    console.log(tempforecast3);
+    var windforecast3 = data.list[21].wind.speed;
+    console.log(windforecast3);
+    var humidityforecast3 =  data.list[21].main.humidity;
+    console.log(humidityforecast3);
+
+    var tempforecast4 = data.list[29].main.temp;
+    console.log(tempforecast4);
+    var windforecast4 = data.list[29].wind.speed;
+    console.log(windforecast4);
+    var humidityforecast4 =  data.list[29].main.humidity;
+    console.log(humidityforecast4);
+
+    var tempforecast5 = data.list[37].main.temp;
+    console.log(tempforecast5);
+    var windforecast5 = data.list[37].wind.speed;
+    console.log(windforecast5);
+    var humidityforecast5 =  data.list[37].main.humidity;
+    console.log(humidityforecast5);
+    }
+};
+
 
 
 ///////////////////////////////////////
@@ -99,10 +139,10 @@ var logCity = function (name) {
 // data pulled for current forecast - plugged into class - todaysCast
 
 
-// const restorequeryURL1 = JSON.parse(localStorage.getItem("queryURL1"));
+const restorequeryURL1 = JSON.parse(localStorage.getItem("queryURL1"));
 
 ///////////////////////
 //data pulled for five day forecast  - plugged into class - fiveDaysCast
 
 
-// const restorequeryURL2 = JSON.parse(localStorage.getItem("queryURL2"));
+const restorequeryURL2 = JSON.parse(localStorage.getItem("queryURL2"));
